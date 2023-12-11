b71e8fe21c96705bacb82e7ad91cd6544f0de7fb
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-12-11 11:16:55 +0530

    Update index.js

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -2,6 +2,7 @@ const express=require('express')
 const Router=express.Router()
 
 // this is the entry point of all the api/v1/questions named url's
+
 Router.use('/question',require('./questions'));
 Router.use('/options',require('./options'))
 
