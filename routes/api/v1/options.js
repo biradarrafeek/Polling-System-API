19534aa375b2f3fb9ac84d21cc303d95b53cedf2
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-12-11 11:17:59 +0530

    Update options.js

--- routes/api/v1/options.js
+++ routes/api/v1/options.js
@@ -2,6 +2,7 @@ const express=require('express')
 const Router=express.Router()
 const optionsController=require('../../../controller/OptionsController');
 
+
 Router.post('/:id/create',optionsController.create);
 Router.get('/:id/add_vote',optionsController.add_vote);
 Router.delete('/delete/:id',optionsController.delete)
