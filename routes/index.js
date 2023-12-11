327ff2f5fb5d0d5cc32e7c8bca65d73b13b37910
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-12-11 11:15:01 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express=require('express')
 const Router=express.Router()
 
 // this is the entry point of all the api named url's
+
 console.log("i m in")
 Router.use('/api',require('./api/index'));
-
 module.exports=Router
