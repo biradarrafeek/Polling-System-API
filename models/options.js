9c5d9e66f6c08b4934432d84618764311b0b027c
Rafeek Biradar <90768585+biradarrafeek@users.noreply.github.com>
2023-12-11 11:12:28 +0530

    Update options.js

--- models/options.js
+++ models/options.js
@@ -6,18 +6,21 @@ const optionSchema=new mongoose.Schema({
         type:String,
         required:true
     },
+    
     question:{
         type:mongoose.Schema.Types.ObjectId,
         required:true
     },
+    
     vote:{
         type:Number,
         default:0
     },
+    
     add_vote:{
         type:String,
     }
 })
-
 const Option=mongoose.model('Option',optionSchema);
 module.exports=Option
+
